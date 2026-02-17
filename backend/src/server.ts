6e02a41e31a0ad77a9b8187dc15406aac2a4f379
moin-trueinception <moin.trueinception.in@gmail.com>
2026-02-17 11:46:26 +0530

    refactor: clean up server.ts by removing unused imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import app from "./app"
-// import "./dummy"
 import connectDb from "./config/db"
-import { sendEmail } from "./utils/mailer";
 
 const PORT = process.env.PORT || 5000;
 
